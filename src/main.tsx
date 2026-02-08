3ff9af862f771182d3981d087c04f63d8a9d3366
Novashin <mproject1026@gmail.com>
2026-02-08 22:25:31 +0800

    Fix: Final Vercel build configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css' // 這一行非常重要！
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
